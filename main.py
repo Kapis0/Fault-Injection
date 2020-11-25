df8fc1f566b71277e2f81990a287f39bf2ad03b9
Francesco Capasso <74915615+Kapis0@users.noreply.github.com>
2020-11-25 20:06:38 +0100

    Add files via upload
    ########## File config.json ###########
    
    Aggiunto CREATE_DIR con id = 1
    Modificato l'iniezione nel superblocco NON Random con aggiunta dei parametri bytes, count e seek.
    
    ########## File main.py ###########
    Creazione delle Classi Event, Injection e Partition
    Da migliorare la lista eventi per l'estrazione dell'evento
    Provata una prima simulazione con iniezione nel superblocco in modo non random e in automatico ( in questo caso l'utente deve solo digitare Y e N)

--- main.py
+++ main.py
@@ -1,165 +1,124 @@
 import os
 import json
-from random import random
 
-
-# Il vantaggio dell'utilizzo di with è che non è necessario chiudere esplicitamente
-# i file o preoccuparsi dei casi in cui è presente un'eccezione.
-def crea_10files():
-    for item in range(1, 10):
-        with open("/mnt/file{}.txt".format(item), "w") as file:
-            file.write("This is file {}\n".format(item))
-    print("\n*** Sto creando i 10 file ***")
-
-
-def crea_2files():
-    for item in range(1, 3):
-        with open("/mnt/dir1/file{}.txt".format(item), "w") as file:
-            file.write("This is file {}\n".format(item))
-    print("\n*** Sto creando i 2 file ***")
-
-
-def crea_directory(path):
-    print("\n *** Sto creando la directory *** ")
-    os.mkdir(path)
-
-
-def elimina_file():
-    for item in range(1, 10):
-        with open("/mnt/file{}.txt".format(item), "r"):
-            os.remove("/mnt/file{}.txt".format(item))
-    print("\n*** Ho rimosso i file ***")
-
-
-def rimuovi_directory(path_directory):
-    print("\n *** Sto rimuovendo la directory")
-    os.system("rm -rf " + path_directory)
-
-
-def mostra_contenuto(path_directory):
-    print("\n *** Sto mostrando il conenuto della directory")
-    os.system("ls -l " + path_directory)
-
-def mostra_contenuto_file():
-    for item in range(1, 3):
-        with open("/mnt/dir1/file{}.txt".format(item), "r") as file:
-            print(file.read().format(item))
-
-
-def smontaggio(partizione):
-    print("\n*** Sto smontando la partizione ***")
-    os.system("umount " + partizione)
-
-
-def montaggio(partizione, parent_dir):
-    print("\n*** Sto montando la partizione in " + parent_dir + " ***")
-    os.system("mount " + partizione + " " + parent_dir)
-
-
-def formattazione(partizione):
-    print("\n*** Sto formattando la partizione ***")
-    os.system("mkfs.ext4 " + partizione)
-
-
-def iniezione_superblocco(partizione):
-    print("\n*** Sto procedendo con l'iniezione ***")
-    os.system("dd if=/dev/zero of=" + partizione + " count=1 bs=1024 seek=1")
-
-
-def filesystem_check(partizione):
-    os.system("e2fsck " + partizione)
-
-
-partizione = "/dev/sdb1"
-parent_dir = "/mnt"
-directory = "dir1"
-path = os.path.join(parent_dir, directory)  # /mnt/dir1
-lost_found = "lost+found"
-path_lost = os.path.join(parent_dir, lost_found)  # /mnt/lost+found
-
-
-# read file
 myjsonfile = open('config.json', 'r')
 data = myjsonfile.read()
 obj = json.loads(data)
-list = obj['faults']
+glo = obj['globals']
+fut = glo['fail_futex']
+usr = obj['user_activity']
+injs = obj['injections']
+par2 = [injs[0].get("parameters")]
+par3 = [injs[1].get("parameters")]
+BYTE_2 = par2[0].get("bytes")
+COUNT_2 = par2[0].get("count")
+SEEK_2 = par2[0].get("seek")
+
+list_event = []
+iterator = iter(list_event)
+NAME_DEV = "/dev/sdb1"
+NAME_DIR = "/mnt"
+DEV_ZERO = "/dev/zero"
+DEV_RANDOM = "/dev/urandom"
+
+class Event:
+
+    cont = 0
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        Event.cont += 1
+
+    def __repr__(self):
+        return "\n{} --> {}".format(self.name, self.id)
+
+    def to_string(self):
+        return f"{self.id}"
 
 
-# 1
-def corruzione_superblocco(list):
-    smontaggio(partizione)
-    formattazione(partizione)
-    montaggio(partizione, parent_dir)
-    crea_10files()
-    # print(list[0])
-    rand = random()
-    # print(rand)
-    if list[2].get("value") == 0:
-        if rand < list[0].get("value"):
-            print("\n***** Sto iniettando il fault con probabilità " + str(list[0].get("value")) + " *****")
-            iniezione_superblocco(partizione)
-        else:
-            print("\n***** Iniezione non andata a buon fine ***** ")
-    smontaggio(partizione)
-    montaggio(partizione, parent_dir)  # dovrebbe fallire se rand > probabilità
-    risp = input("\n***** Il montaggio è fallito? {s,n} ")
-    if (risp == "s"):
-        print("\n***** Sto avviando e2fsck per la riparazione")
-        filesystem_check(partizione)
+class Injection:
+
+    def __init__(self, fileIN, fileOUT, bs, count, seek):
+        self.fileIN = fileIN
+        self.fileOUT = fileOUT
+        self.bs = bs
+        self.count = count
+        self.seek = seek
+
+    def __repr__(self):
+        return "\n{} --> {} with {} {} {}".format(self.fileIN, self.fileOUT, self.bs, self.count, self.seek)
+
+    def injection_superblock(self):
+        print("\n*** Loading Fault injection ***")
+        os.system("dd if=" + self.fileIN +" of=" + self.fileOUT + " bs=" + str(self.bs) +" count=" + str(self.count) + " seek=" + str(self.seek) +"")
 
 
-# 2
-def corruzione_inode():
-    smontaggio(partizione)
-    formattazione(partizione)
-    montaggio(partizione, parent_dir)
-    rimuovi_directory(path_lost)
-    rimuovi_directory(path)
-    crea_directory(path)
-    inode_dir = os.stat(path).st_ino
-    # print(str(inode_dir))
-    # os.system("ls -i /mnt")
-    crea_2files()
-    # os.system("cat /mnt/dir1/file1.txt /mnt/dir1/file2.txt")
-    os.system("debugfs -R 'clri <" + str(inode_dir) + ">' " + partizione + " -w")
-    smontaggio(partizione)
-    montaggio(partizione, parent_dir)
-    mostra_contenuto(path)  # dovrebbe fallire e chiedere di fare pulizia
-    smontaggio(partizione)
-    filesystem_check(partizione)
-    montaggio(partizione, parent_dir)
-    mostra_contenuto(path_lost)
-    # os.system("rm -rf /mnt/lost+found")
+class Partition:
+
+    def __init__(self, dev, dir):
+        self.dev = dev
+        self.dir = dir
+
+    def __repr__(self):
+        return "\n{} --> {}".format(self.dev, self.dir)
+
+    def unmounting(self):
+        print("\n*** Unmounting the partition ***")
+        os.system("umount " + str(self.dev))
+
+    def mounting(self):
+        print("\n*** Mounting "+ self.dev +" in " + self.dir + " ***")
+        os.system("mount " + self.dev + " " + self.dir)
+
+    def formatting(self):
+        print("\n*** Partition formatting ***")
+        os.system("mkfs.ext4 " + self.dev)
+
+    def filesystem_check(self):
+        os.system("e2fsck " + self.dev)
 
 
-# 3
-def corruzione_blocco():
-    smontaggio(partizione)
-    formattazione(partizione)
-    montaggio(partizione, parent_dir)
-    crea_10files()
-    os.system("cat /mnt/file1.txt /mnt/file2.txt")
-    os.system("debugfs -R 'stat <12>' " + partizione + "|grep '(0)'")
-    os.system("debugfs -R 'mi <12>' " + partizione + " -w")
-    smontaggio(partizione)
-    montaggio(partizione, parent_dir)
-    os.system("cat /mnt/file1.txt /mnt/file2.txt")
-    smontaggio(partizione)
-    filesystem_check(partizione)
+if __name__ == '__main__':
 
+    if glo.get("user_activity") == True and fut[0].get("fail_futex") == 1:
+        ev0 = Event(usr[0].get("type"), usr[0].get("id"))
+        print(ev0)
 
-while True:
-    try:
-        n = int((input("\nDove vuoi iniettare il fault?\n1. Superblocco\n2. I-node\n3. Blocco\n")))
-        break
-    except ValueError:
-        print("\nScelta non valida")
+    if glo.get("user_activity") == True and fut[1].get("fail_futex") == 1:
+        ev1 = Event(usr[1].get("type"), usr[1].get("id"))
+        print(ev1)
 
-if n == 1:
-    corruzione_superblocco(list)
-elif n == 2:
-    corruzione_inode()
-elif n == 3:
-    corruzione_blocco()
-else:
-    print("\nScelta non valida")
+    if fut[2].get("fail_futex") == 1:
+        ev2 = Event(injs[0].get("type"), injs[0].get("id"))
+        print(ev2)
+
+    if fut[3].get("fail_futex") == 1:
+        ev3 = Event(injs[1].get("type"), injs[1].get("id"))
+        print(ev3)
+
+    p = Partition(NAME_DEV, NAME_DIR)
+
+    while True:
+        try:
+            num = int((input("\nChoose an injection's type\n1. Superblock\n2. I-node\n3. Block\n")))
+            break
+        except ValueError:
+            print("\nInvalid input")
+
+    if num == 1:
+        r = input("\n Do you want inject random? {y,n} ")
+        if r == "n":
+            list_event.append(ev2.to_string())
+            list_event.append(ev3.to_string())
+            if next(iterator) == str(ev2.to_string()):
+                fault = Injection(DEV_ZERO, NAME_DEV, BYTE_2, COUNT_2, SEEK_2)
+                p.unmounting()
+                p.mounting()
+                fault.injection_superblock()
+                p.unmounting()
+                p.mounting()
+                risp = input("\n***** Mounting failed? {y,n} ")
+                if (risp == "y"):
+                    risp2 = input("\n***** Run e2fsck? {y,n} ")
+                    if (risp2 == "y"):
+                        p.filesystem_check()
