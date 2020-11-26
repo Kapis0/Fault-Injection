183bdb02ed9a68e5ed4efe30427a31bfc8fd836a
Francesco Capasso <74915615+Kapis0@users.noreply.github.com>
2020-11-26 17:51:12 +0100

    Update main.py
    Il programma fa un primo controllo su “user activity” e “fail futex” , se entrambe sono verificate vengono creati i file e la directory.
    Successivamente le iniezioni se abilitate (sempre in base a “fail futex”) vengono inserite in una lista. 
    Per ora ho lavorato sul Superblocco ma credo sia estendibile anche alle altre due iniezioni.
    Viene data priorità all’iniezione che l’utente sceglie, se fare l’iniezione random (per ora è una stampa) o quella classica.
    Purtroppo il metodo next mi ha dato diversi problemi in quanto non riuscivo ad inserirlo all’interno di un ciclo, quindi ho proceduto come si può vedere da riga 129.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+from random import random
 
 myjsonfile = open('config.json', 'r')
 data = myjsonfile.read()
@@ -15,26 +16,62 @@ COUNT_2 = par2[0].get("count")
 SEEK_2 = par2[0].get("seek")
 
 list_event = []
-iterator = iter(list_event)
+#iterator = iter(list_event)
 NAME_DEV = "/dev/sdb1"
 NAME_DIR = "/mnt"
+NAME_DIR1 = "dir1"
 DEV_ZERO = "/dev/zero"
 DEV_RANDOM = "/dev/urandom"
+PATH = os.path.join(NAME_DIR, NAME_DIR1) #/mnt/dir1
+
+class Partition:
+
+    def __init__(self, dev, dir, path):
+        self.dev = dev
+        self.dir = dir
+        self.path = path
+
+    def __repr__(self):
+        return "\n{} --> {}".format(self.dev, self.dir)
+
+    def create_files(self):
+        for item in range(0, usr[0].get("tasks")):
+            with open("" + self.dir + "/file{}.txt".format(item), "w") as file:
+                file.write("This is file {}\n".format(item))
+        print("\n*** Creation of the " + str(usr[0].get("tasks")) + " files ***")
+
+    def create_directory(self):
+        print("\n *** Directory creation *** ")
+        os.mkdir(self.path)
+
+    def unmounting(self):
+        print("\n*** Unmounting the partition ***")
+        os.system("umount " + str(self.dev))
+
+    def mounting(self):
+        print("\n*** Mounting "+ self.dev +" in " + self.dir + " ***")
+        os.system("mount " + self.dev + " " + self.dir)
+
+    def formatting(self):
+        print("\n*** Partition formatting ***")
+        os.system("mkfs.ext4 " + self.dev)
+
+    def filesystem_check(self):
+        os.system("e2fsck " + self.dev)
 
 class Event:
 
-    cont = 0
+    #cont = 0
     def __init__(self, name, id):
         self.name = name
         self.id = id
-        Event.cont += 1
+        #Event.cont += 1
 
     def __repr__(self):
         return "\n{} --> {}".format(self.name, self.id)
 
     def to_string(self):
         return f"{self.id}"
-
 
 class Injection:
 
@@ -53,50 +90,34 @@ class Injection:
         os.system("dd if=" + self.fileIN +" of=" + self.fileOUT + " bs=" + str(self.bs) +" count=" + str(self.count) + " seek=" + str(self.seek) +"")
 
 
-class Partition:
-
-    def __init__(self, dev, dir):
-        self.dev = dev
-        self.dir = dir
-
-    def __repr__(self):
-        return "\n{} --> {}".format(self.dev, self.dir)
-
-    def unmounting(self):
-        print("\n*** Unmounting the partition ***")
-        os.system("umount " + str(self.dev))
-
-    def mounting(self):
-        print("\n*** Mounting "+ self.dev +" in " + self.dir + " ***")
-        os.system("mount " + self.dev + " " + self.dir)
-
-    def formatting(self):
-        print("\n*** Partition formatting ***")
-        os.system("mkfs.ext4 " + self.dev)
-
-    def filesystem_check(self):
-        os.system("e2fsck " + self.dev)
-
-
 if __name__ == '__main__':
+
+    p = Partition(NAME_DEV, NAME_DIR, PATH)
 
     if glo.get("user_activity") == True and fut[0].get("fail_futex") == 1:
         ev0 = Event(usr[0].get("type"), usr[0].get("id"))
+        p.unmounting()
+        p.mounting()
+        p.create_files()
         print(ev0)
 
     if glo.get("user_activity") == True and fut[1].get("fail_futex") == 1:
         ev1 = Event(usr[1].get("type"), usr[1].get("id"))
+        p.unmounting()
+        p.mounting()
+        p.create_directory()
         print(ev1)
 
     if fut[2].get("fail_futex") == 1:
         ev2 = Event(injs[0].get("type"), injs[0].get("id"))
+        list_event.append(ev2.to_string())
         print(ev2)
 
     if fut[3].get("fail_futex") == 1:
         ev3 = Event(injs[1].get("type"), injs[1].get("id"))
+        list_event.append(ev3.to_string())
         print(ev3)
 
-    p = Partition(NAME_DEV, NAME_DIR)
 
     while True:
         try:
@@ -107,18 +128,44 @@ if __name__ == '__main__':
 
     if num == 1:
         r = input("\n Do you want inject random? {y,n} ")
+
+        if r == "y":
+            try:
+                list_event.remove(ev3.to_string())
+                list_event.insert(0, ev3.to_string())
+            except ValueError:
+                pass
+
         if r == "n":
-            list_event.append(ev2.to_string())
-            list_event.append(ev3.to_string())
-            if next(iterator) == str(ev2.to_string()):
-                fault = Injection(DEV_ZERO, NAME_DEV, BYTE_2, COUNT_2, SEEK_2)
-                p.unmounting()
-                p.mounting()
-                fault.injection_superblock()
-                p.unmounting()
-                p.mounting()
-                risp = input("\n***** Mounting failed? {y,n} ")
-                if (risp == "y"):
-                    risp2 = input("\n***** Run e2fsck? {y,n} ")
-                    if (risp2 == "y"):
-                        p.filesystem_check()
+            try:
+                list_event.remove(ev2.to_string())
+                list_event.insert(0, ev2.to_string())
+            except ValueError:
+                pass
+
+    while len(list_event) != 0:
+        if list_event[0] == ev2.to_string():
+            #if next(iterator) == str(ev2.to_string()):
+            fault = Injection(DEV_ZERO, NAME_DEV, BYTE_2, COUNT_2, SEEK_2)
+            p.unmounting()
+            p.mounting()
+            fault.injection_superblock()
+            p.unmounting()
+            p.mounting()
+            risp = input("\n***** Mounting failed? {y,n} ")
+            if (risp == "y"):
+                risp2 = input("\n***** Run e2fsck? {y,n} ")
+                if (risp2 == "y"):
+                    p.filesystem_check()
+            list_event.remove(ev2.to_string())
+
+        elif list_event[0] == ev3.to_string():
+            #if next(iterator) == str(ev3.to_string()):
+            print("Iniezione Random")
+            list_event.remove(ev3.to_string())
+            #print(list_event)
+
+
+
+
+
