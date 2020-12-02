09869136d3f92e193d43698ee8f3383489336013
Francesco Capasso <74915615+Kapis0@users.noreply.github.com>
2020-12-02 15:20:48 +0100

    Create prints_events.py

---
+++ fault_injection/prints_events.py
@@ -0,0 +1,13 @@
+from fault_injection.configuration import Configuration
+from fault_injection.schedule import Scheduler
+
+if __name__ == "__main__":
+    config = Configuration("config.json")
+    config.load()
+    sched = Scheduler()
+    sched.load_injections(config.get_injections())
+
+    ev = sched.next_event()
+    while ev is not None:
+        print(ev)
+        ev = sched.next_event()
