90f72d47b09f4c2fc93b6ad70473ee9443d0ac6f
Francesco Capasso <74915615+Kapis0@users.noreply.github.com>
2020-12-02 16:40:23 +0100

    Update injector.py

--- fault_injection/injector.py
+++ fault_injection/injector.py
@@ -1,13 +1,12 @@
 from fault_injection.main import InjectionS, InjectionID
 from fault_injection.schedule import Event
 
-class Injector(Event):
-    
+class Injector(Event, InjectionS, InjectionID):
+
     def __init__(self):
         Event.__init__(self, self.occur_time, self.type, self.parameters)
         
-    def inject(self, injection):
-        self.injection = injection
+    def inject(self):
         
         if self.type == "superblock_corruption":
             InjectionS.injection_superblock(self)
@@ -17,4 +16,5 @@ class Injector(Event):
             InjectionID.injection_inode(self)
         if self.type == "direct_block_corruption":
             InjectionID.injection_directblock(self)
+
             
