f35aea9b685e1afe89d7f97e0e136decc81b7284
Francesco Capasso <74915615+Kapis0@users.noreply.github.com>
2020-12-02 15:19:33 +0100

    Create configuration.py

---
+++ fault_injection/configuration.py
@@ -0,0 +1,30 @@
+import json
+
+
+class Configuration:
+    config_file = None
+    jsonObject = None
+
+    def __init__(self, filename):
+        self.config_file = filename
+
+    def load(self):
+        if self.config_file is not None:
+            fp = open(self.config_file)
+            self.jsonObject = json.load(fp)
+        else:
+            raise Exception("Config File Not Initialized")
+
+    def get_injections(self):
+        if self.jsonObject is None:
+            self.load()
+
+        fail_futex = {}
+        for futex in self.jsonObject["globals"]['fail_futex']:
+            fail_futex[futex['id']] = futex['fail_futex']
+
+        injections = []
+        for injection in self.jsonObject["injections"]:
+            if fail_futex[injection['id']] == 1:
+                injections.append(injection)
+        return injections
