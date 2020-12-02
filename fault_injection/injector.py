be296fd9a5c07fd3da24415e5845f0dcb72468e7
Francesco Capasso <74915615+Kapis0@users.noreply.github.com>
2020-12-02 18:26:49 +0100

    Update injector.py

--- fault_injection/injector.py
+++ fault_injection/injector.py
@@ -2,14 +2,13 @@ import os
 import json
 import random
 from fault_injection.fault import InjectionS, InjectionID
-from fault_injection.schedule import Event
+from fault_injection.schedule import Event, Scheduler
 
 # READ JSON
 JSON_FILE = open('config.json', 'r')
-DATA = JSON_FILE.read()
 
 # PARSE
-OBJECT = json.loads(DATA)
+OBJECT = json.loads(JSON_FILE.read())
 
 # SUPERBLOCK PARAMETERS
 BYTE_ALL = OBJECT["injections"][0]["parameters"]["bytes"]
@@ -32,17 +31,20 @@ PATH_LOST = os.path.join(PARENT_DIR, LOST_FOUND)  # /mnt/lost+found
 PATH_FILE0 = os.path.join(PARENT_DIR, FILE0)  # /mnt/file0.txt
 
 
-class Injector(Event, InjectionS, InjectionID):
+class Injector(Event):
 
-    def __init__(self, injections):
+    def __init__(self):
         Event.__init__(self, self.occur_time, self.type, self.parameters)
+
+    injections = None
+
+    def inject(self, injections):
+        
         self.injections = injections
-
-    def inject(self):
-
-        for injection in self.injections:
+       
+        for injection in injections:
             self.type = injection["type"]
-
+            
             if self.type == "superblock_corruption":
                 fault = InjectionS(DEV_USB, PARENT_DIR, DEV_ZERO, DEV_USB, BYTE_ALL, COUNT, SEEK)
                 fault.injection_superblock()
@@ -57,5 +59,6 @@ class Injector(Event, InjectionS, InjectionID):
                 INODE_FILE = os.stat(PATH_FILE0).st_ino
                 fault_directblock = InjectionID(DEV_USB, PARENT_DIR, INODE_FILE, None, None)
                 fault_directblock.injection_directblock()
+
 
             
