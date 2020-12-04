952c5364a6dd4f7b68f65662047627503d1dfe04
Francesco Capasso <74915615+Kapis0@users.noreply.github.com>
2020-12-04 15:47:36 +0100

    Update prints_events.py

--- fault_injection/prints_events.py
+++ fault_injection/prints_events.py
@@ -1,16 +1,24 @@
 from fault_injection.configuration import Configuration
-from fault_injection.schedule import Scheduler
+from fault_injection.schedule import Scheduler, Scheduler_User
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
 
 if __name__ == "__main__":
     config = Configuration("config.json")
     config.load()
+
+    usr = Scheduler_User()
+    usr.load_user_activity(config.get_user_activities())
+    usr.start()
+    usr.join()
+    usr.sched_user_activity_nowait()
+
     sched = Scheduler()
     sched.load_injections(config.get_injections())
+    sched.start()
+    sched.join()
+    sched.sched_injections_nowait()
 
-    ev = sched.next_event()
-    while ev is not None:
-        print(ev)
-        ev = sched.next_event()
-        
-    inj = Injector()
-    inj.inject(config.get_injections())
+
+
