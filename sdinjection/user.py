0619fca51c4f06eba06b4be0e5512e4db1d96e73
Francesco Capasso <74915615+Kapis0@users.noreply.github.com>
2020-12-29 19:13:20 +0100

    Update user.py

--- sdinjection/user.py
+++ sdinjection/user.py
@@ -1,7 +1,6 @@
 import os
-from sdinjection.utils import Partition
+from sdinjections.utils import Path
 
-DEV_USB = "/dev/sdb1"
 PARENT_DIR = "/mnt"
 DIR1 = "dir1"
 PATH_DIR1 = os.path.join(PARENT_DIR, DIR1)  # /mnt/dir1
@@ -19,23 +18,20 @@ class User:
     @staticmethod
     def __real_user_activity(activity):
 
-        p = Partition(DEV_USB, PARENT_DIR)
-        p_dir = Partition(DEV_USB, PATH_DIR1)
-
         if activity["type"] == "CREATE_FILES":
-            p.umount_mount()
-            p.create_files(PARENT_DIR)
+            p = Path(PARENT_DIR)
+            p.create_files(PARENT_DIR, activity["tasks"])
 
         elif activity["type"] == "CREATE_DIR":
-            p.umount_mount()
+            p_dir = Path(PATH_DIR1)
             p_dir.remove_directory(PATH_DIR1)
             p_dir.create_directory(PATH_DIR1)
-            p_dir.create_files(PATH_DIR1)
+            p_dir.create_files(PATH_DIR1, activity["tasks"])
 
     @staticmethod
     def __simulate(activity):
         if activity["type"] == "CREATE_FILES":
-            print('User Activity: ' + activity["type"] + 'parameters:', DEV_USB, PARENT_DIR)
+            print('User Activity: ' + activity["type"] + 'parameters:', PARENT_DIR)
 
         elif activity["type"] == "CREATE_DIR":
-            print('User Activity: ' + activity["type"] + 'parameters:', DEV_USB, PATH_DIR1)
+            print('User Activity: ' + activity["type"] + 'parameters:', PATH_DIR1)
